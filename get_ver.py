3ff00d2b5f07e7d9d336fbd2c78fca3065fcc7cb
ChrisChrisLoLo <sporewoh@gmail.com>
2021-01-13 19:48:15 -0700

    Complete step 9-11

--- get_ver.py
+++ get_ver.py
@@ -1,5 +1,12 @@
 import requests
+
+# Print version
 # print(requests.__version__)
 
-r = requests.get('https://www.google.com')
-print(r)
+# Print request from google
+# r = requests.get('https://www.google.com')
+# print(r)
+
+# Get this exact code from github
+r = requests.get('https://raw.githubusercontent.com/ChrisChrisLoLo/CMPUT404Lab1/main/get_ver.py')
+print(r.text)
